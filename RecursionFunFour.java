73b0561c5a367670578763cc0fe0a43d6577e65b
svolp <143809611+svolp@users.noreply.github.com>
2025-03-24 09:11:55 -0400

    Add files via upload

---
+++ RecursionFunFour.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - 
+
+import static java.lang.System.*;
+
+public class RecursionFunFour
+{
+	//sum all odd digits
+	public static int sumOddDigits(int num)
+	{
+		return 0;
+	}
+}
---
+++ RecursionRunner.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - 
+
+import static java.lang.System.*;
+
+public class RecursionRunner
+{
+	public static void main(String args[])
+	{
+		System.out.println(RecursionFunOne.countOddDigits(453222));
+		
+		//add more test cases
+	}
+}
